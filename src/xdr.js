6d87ede83e3be590f47f5c5a6f345a57c99dfbf9
Brandon Nielsen <nielsenb@jetfuse.net>
2016-01-13 16:44:25 -0600

    Ensure 'map' has a values property before getting its values.
    This works around issues with interpreters that loop over all array
    properties, instead of just indices, when used with a for-in loop.

--- src/xdr.js
+++ src/xdr.js
@@ -43,9 +43,11 @@ function dapUnpacker(xdrdata, dapvar) {
             out.push(tmp);
 
             for (var map in dapvar.maps) {
-                this.dapvar = dapvar.maps[map];
-                tmp = this.getValue();
-                out.push(tmp);
+                if (dapvar.maps[map].type) {
+                    this.dapvar = dapvar.maps[map];
+                    tmp = this.getValue();
+                    out.push(tmp);
+                }
             }
 
             this.dapvar = dapvar;
