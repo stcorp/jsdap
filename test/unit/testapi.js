b00aebc09b373f5875db1fbe2b321522f3f64725
Brandon Nielsen <nielsenb@jetfuse.net>
2016-03-30 13:30:34 -0500

    Add a proxy test.

--- test/unit/testapi.js
+++ test/unit/testapi.js
@@ -70,7 +70,6 @@ describe('api functions', function() {
 
             dummyDDSParser.parse = function(dds) {
                 //TODO: Check DDS
-                //expect(dds).toEqual('dummyDDSResponseBody');
                 return 'dummyDDSParsedDataset';
             };
 
@@ -88,6 +87,92 @@ describe('api functions', function() {
             }
 
             jsdap.loadDataset(dummyUrl, dummyCallback, null);
+
+            //Trigger the state changes
+            dummyXMLHttpRequest.onreadystatechange();
+            dummyXMLHttpRequest.onreadystatechange();
+        });
+
+        it('should make a correct proxyUrl request when a proxy is defined', function() {
+            var globalReference = null; //'window' for the browser, global for Node.js
+            var dummyXMLHttpRequest = Object();
+            var dummyDDSParser = Object();
+            var dummyDASParser = Object();
+
+            var dummyUrl = 'http://test.com/test.nc';
+            var dummyProxy = 'http://testProxy.com';
+
+            if (typeof window !== 'undefined') {
+                globalReference = window;
+            }
+            else {
+                globalReference = global; //Node.js
+            }
+
+            spyOn(globalReference, 'XMLHttpRequest').and.callFake(function() {
+                return dummyXMLHttpRequest;
+            });
+
+            dummyXMLHttpRequest.open = function(method, url, async) {
+                expect(method).toEqual('GET');
+                expect(async).toEqual(true);
+
+                if (url === dummyProxy + '?url=' + encodeURIComponent(dummyUrl + '.dds')) {
+                    dummyXMLHttpRequest.setRequestHeader = function(header, value) {
+                        expect(header).toEqual('Accept-Charset');
+                        expect(value).toEqual('x-user-defined');
+                    };
+
+                    dummyXMLHttpRequest.send = function(data) {
+                        expect(data).toEqual('');
+                    };
+
+                    dummyXMLHttpRequest.readyState = XML_READY_STATE_DONE;
+
+                    dummyXMLHttpRequest.responseText = 'dummyDDSResponseBody';
+                }
+                else if (url === dummyProxy + '?url=' + encodeURIComponent(dummyUrl + '.das')) {
+                    dummyXMLHttpRequest.setRequestHeader = function(header, value) {
+                        expect(header).toEqual('Accept-Charset');
+                        expect(value).toEqual('x-user-defined');
+                    };
+
+                    dummyXMLHttpRequest.send = function(data) {
+                        expect(data).toEqual('');
+                    };
+
+                    dummyXMLHttpRequest.readyState = XML_READY_STATE_DONE;
+
+                    dummyXMLHttpRequest.responseText = 'dummyDASResponseBody';
+                }
+                else {
+                    throw new Error('Unrecognized url ' + url);
+                }
+            };
+
+            spyOn(parser, 'ddsParser').and.callFake(function() {
+                return dummyDDSParser;
+            });
+
+            dummyDDSParser.parse = function(dds) {
+                //TODO: Check DDS
+                return 'dummyDDSParsedDataset';
+            };
+
+            spyOn(parser, 'dasParser').and.callFake(function() {
+                return dummyDASParser;
+            });
+
+            dummyDASParser.parse = function(das, dataset) {
+                //TODO: Check DAS, dataset
+                return 'dummyDASParsedDataset';
+            };
+
+            function dummyCallback(data) {
+                expect(data).toEqual('dummyDASParsedDataset');
+            }
+
+            jsdap.loadDataset(dummyUrl, dummyCallback, dummyProxy);
 
             //Trigger the state changes
             dummyXMLHttpRequest.onreadystatechange();
