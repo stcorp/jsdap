37dc92c201e8e39bf9ab0125104f49b282c057c3
Paul Brussee <paul.brussee@stcorp.nl>
2019-10-28 19:52:38 +0100

    Release v10

--- src/api.js
+++ src/api.js
@@ -1,136 +1,137 @@
 var jsdap = {};
 
 if (typeof require !== 'undefined' && module.exports) {
-    parser = require('./parser');
-    xdr = require('./xdr');
+    var parser = require('./parser');
+    var xdr = require('./xdr');
 
     //Workaround infinite recursion when jsdap is included in a webpack project
     if (typeof XMLHttpRequest === 'undefined') {
-        XMLHttpRequest = require('xhr2');
+        var XMLHttpRequest = require('xhr2');
     }
 }
 
 (function() {
     'use strict';
 
-    var proxyUrl = function(url, callback, binary) {
-        var xml = new XMLHttpRequest();
+    jsdap.newRequest = function(url, binary) {
+        var xhr = new XMLHttpRequest();
 
-        xml.open('GET', url, true);
+        xhr.open('GET', url, true);
 
         if (binary) {
-            xml.responseType = 'arraybuffer';
+            xhr.responseType = 'arraybuffer';
         }
         else {
-            if (xml.overrideMimeType) {
-                xml.overrideMimeType('text/plain; charset=x-user-defined');
+            if (xhr.overrideMimeType) {
+                xhr.overrideMimeType('text/plain; charset=x-user-defined');
             }
             else {
-                xml.setRequestHeader('Accept-Charset', 'x-user-defined');
+                xhr.setRequestHeader('Accept-Charset', 'x-user-defined');
             }
         }
 
-        xml.onload = function() {
-            if (binary) {
-                var buf =
-                       xml.responseBody             //XHR2
-                    || xml.response                 //FF7/Chrome 11-15
-                    || xml.mozResponseArrayBuffer;  //FF5
+        return xhr;
+    };
 
-                callback(buf);
+    jsdap.dodsRequestHandler = function(xhr) {
+        var dods = xhr.responseBody             //XHR2
+                || xhr.response                 //FF7/Chrome 11-15
+                || xhr.mozResponseArrayBuffer;  //FF5
+
+        var dataStart = '\nData:\n';
+        var view = new DataView(dods);
+        var byteIndex = 0;
+
+        var dds = ''; //The DDS string
+
+        while (byteIndex < view.byteLength) {
+            dds += String.fromCharCode(view.getUint8(byteIndex));
+
+            if (dds.indexOf(dataStart) !== -1) {
+                break;
             }
-            else {
-                callback(xml.responseText);
-            }
+
+            byteIndex += 1;
+        }
+
+        dds = dds.substr(0, dds.length - dataStart.length); //Remove the start of data string '\nData:\n'
+        dods = dods.slice(byteIndex + 1); //Split off the DDS data
+
+        var dapvar = new parser.ddsParser(dds).parse();
+        var data = new xdr.dapUnpacker(dods, dapvar).getValue();
+
+        //Return an object containing the DDS for the requested data, as well as the requested data
+        return {
+            dds: dapvar,
+            data: data,
         };
-
-        xml.send(null);
     };
 
-    var dodsRequest = function(url, callback) {
-        //Returns an object containing the DDS for the requested data, as well as the requested data
-        proxyUrl(url, function(dods) {
-            var dataStart = '\nData:\n';
-            var view = new DataView(dods);
-            var byteIndex = 0;
+    jsdap.ddsRequestHandler = function(xhr) {
+        const dds = xhr.responseText;
 
-            var dds = ''; //The DDS string
-
-            while (byteIndex < view.byteLength) {
-                dds += String.fromCharCode(view.getUint8(byteIndex));
-
-                if (dds.indexOf(dataStart) !== -1) {
-                    break;
-                }
-
-                byteIndex += 1;
-            }
-
-            dds = dds.substr(0, dds.length - dataStart.length); //Remove the start of data string '\nData:\n'
-            dods = dods.slice(byteIndex + 1); //Split off the DDS data
-
-            var dapvar = new parser.ddsParser(dds).parse();
-            var data = new xdr.dapUnpacker(dods, dapvar).getValue();
-
-            callback({dds: dapvar, data: data});
-        }, true);
+        return new parser.ddsParser(dds).parse();
     };
 
-    jsdap.loadDataset = function(url, callback, proxy) {
-        //User proxy?
-        if (proxy) url = proxy + '?url=' + encodeURIComponent(url);
+    jsdap.dasRequestHandler = function(xhr, dds = { type: 'Dataset', attributes: {} }) {
+        const das = xhr.responseText;
 
-        //Load DDS.
-        proxyUrl(url + '.dds', function(dds) {
-            var dataset = new parser.ddsParser(dds).parse();
-
-            //Load DAS.
-            proxyUrl(url + '.das', function(das) {
-                dataset = new parser.dasParser(das, dataset).parse();
-                callback(dataset);
-            });
-        });
+        return new parser.dasParser(das, dds).parse();
     };
 
-    jsdap.loadData = function(url, callback, proxy) {
-        //User proxy?
-        if (proxy) url = proxy + '?url=' + encodeURIComponent(url);
+    jsdap.loadDataAndDDS = function(url, onLoad, onError, onAbort, onProgress, onTimeout) {
+        var dodsReq = jsdap.newRequest(url, true);
 
-        dodsRequest(url, function(result) {
-            callback(result.data);
-        });
+        dodsReq.onload = function() {
+            onLoad(jsdap.dodsRequestHandler(dodsReq));
+        };
+        dodsReq.onerror = onError;
+        dodsReq.onabort = onAbort;
+        dodsReq.onprogress = onProgress;
+        dodsReq.ontimeout = onTimeout;
+
+        dodsReq.send(null);
     };
 
-    jsdap.loadDataAndDDS = function(url, callback, proxy) {
-        //User proxy?
-        if (proxy) url = proxy + '?url=' + encodeURIComponent(url);
+    jsdap.loadDDS = function(url, onLoad, onError, onAbort, onProgress, onTimeout) {
+        var ddsReq = jsdap.newRequest(url);
 
-        dodsRequest(url, function(result) {
-            //Return the data and the DDS
-            callback(result);
-        });
+        ddsReq.onLoad = function() {
+            onLoad(jsdap.ddsRequestHandler(ddsReq));
+        };
+        ddsReq.onerror = onError;
+        ddsReq.onabort = onAbort;
+        ddsReq.onprogress = onProgress;
+        ddsReq.ontimeout = onTimeout;
+
+        ddsReq.send(null);
     };
 
-    jsdap.loadDDS = function(url, callback, proxy) {
-         //User proxy?
-        if (proxy) url = proxy + '?url=' + encodeURIComponent(url);
+    jsdap.loadDAS = function(url, dds, onLoad, onError, onAbort, onProgress, onTimeout) {
+        var dasReq = jsdap.newRequest(url);
 
-        //Load DDS
-        proxyUrl(url, function(dds) {
-            var dapvar = new parser.ddsParser(dds).parse();
-            callback(dapvar);
-        });
+        dasReq.onload = function() {
+            onLoad(jsdap.dasRequestHandler(dasReq, dds));
+        };
+        dasReq.onerror = onError;
+        dasReq.onabort = onAbort;
+        dasReq.onprogress = onProgress;
+        dasReq.ontimeout = onTimeout;
+
+        dasReq.send(null);
     };
 
-    jsdap.loadDAS = function(url, dds, callback, proxy) {
-        //User proxy?
-        if (proxy) url = proxy + '?url=' + encodeURIComponent(url);
+    jsdap.loadData = function(url, onLoad, onError, onAbort, onProgress, onTimeout) {
+        jsdap.loadDataAndDDS(url, function(dods) {
+            //Return only data
+            return onLoad(dods.data);
+        }, onError, onAbort, onProgress, onTimeout);
+    };
 
-        //Load DAS
-        proxyUrl(url, function(das) {
-            var dataset = new parser.dasParser(das, dds).parse();
-            callback(dataset);
-        });
+    jsdap.loadDataset = function(url, onLoad, onError, onAbort, onProgress, onTimeout) {
+        jsdap.loadDDS(url + '.dds', function(dds) {
+            jsdap.loadDAS(url + '.das', dds, onLoad, onError, onAbort, onProgress, onTimeout);
+        }, onError, onAbort, onProgress, onTimeout);
     };
 
     if (typeof module !== 'undefined' && module.exports) {
